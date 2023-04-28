13c46ea821ce5c5a735eab754b0981a1c46f3cd7
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-04-28 16:11:53 +0500

    i installed gunicorn

--- drf/settings.py
+++ drf/settings.py
@@ -139,10 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
-
-STATIC_ROOT = [BASE_DIR / 'staticfiles']
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
