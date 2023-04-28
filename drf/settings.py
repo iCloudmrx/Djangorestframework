b7c86cffaf98945149bbabe8e9560b0465167f0c
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-04-28 15:50:31 +0500

    i installed guncorn

--- drf/settings.py
+++ drf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ckg$kvc(3i%sqb^&%g*^dy5##v52u)2uz+0^7^!a&@4^@%(ayo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -139,6 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATIC_ROOT = [BASE_DIR / 'staticfiles']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
