ddf41223c2407ec9e4a86b023f90a5ae54d8ecd6
Nurillo Mahmudjonov <72136850+iCloudmrx@users.noreply.github.com>
2023-04-28 20:15:38 +0500

    update settting.py

--- drf/settings.py
+++ drf/settings.py
@@ -140,10 +140,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 import os
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
